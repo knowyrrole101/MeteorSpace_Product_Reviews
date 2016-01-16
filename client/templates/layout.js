db718b3d3b2160e42afd821cdce5555980046066
Moon Ahmed <planetbarsent@gmail.com>
2016-01-15 16:00:52 -0800

    add star ratings and helper functions ratings

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -4,7 +4,28 @@ Template.registerHelper('truncateText', function(text, length){
   if (text.length > length){
     // Makes sure word is not cut off
     new_text = new_text.substr(0, Math.min(new_text.length, new_text.lastIndexOf(" "))) + " ...."
-  };  
+  };
   return new_text;
   // return new Spacebars.SafeString(new_text);
 })
+Template.registerHelper('starRating', function(rating){
+  var stars='';
+  for(var i=1;i<=rating;i++){
+    stars += "<img class='review-stars' src='/img/star.png'>";
+  };
+  return new Spacebars.SafeString(stars);
+});
+Template.registerHelper('avgRating', function(reviews){
+  var average=0;
+  var stars = '';
+  //Add all reviews up
+  for(var i in reviews){
+    console.log(reviews.length)
+    average += parseInt(reviews[i].rating);
+  }
+  average = Math.floor(average/reviews.length)
+  for(var i=1;i<=average;i++){
+    stars += "<img class='review-stars' src='/img/star.png'>";
+  };
+  return new Spacebars.SafeString(stars)
+});
