dda7958f54c02234e265a29a715f2540fc4387d3
Moon Ahmed <planetbarsent@gmail.com>
2016-01-18 17:47:06 -0800

    Server deploy changes

--- server/common.js
+++ server/common.js
@@ -1,9 +1,16 @@
-if (Meteor.isServer){
-  Meteor.publish('products', function(){
-      return Products.find();
-  });
+Meteor.publish('products', function(){
+  return Products.find();
+});
 
-  Meteor.publish('categories', function(){
-    return Categories.find();
-  });
-}
+Meteor.publish('categories', function(){
+  return Categories.find();
+});
+
+ProductsImages.allow({
+  insert: function(fileId, document){
+    return true;
+  },
+  download: function(fileId, document){
+    return true;
+  }
+});
