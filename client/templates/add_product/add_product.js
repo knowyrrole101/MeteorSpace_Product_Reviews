59c7e97e4280e8a837992fbb92a739d59e018e4f
Moon Ahmed <planetbarsent@gmail.com>
2016-01-19 00:11:47 -0800

    Heroku S3 Config

--- client/templates/add_product/add_product.js
+++ client/templates/add_product/add_product.js
@@ -7,7 +7,6 @@ Template.add_product.events({
     //Product Image
     //Jquery to pull file from form.
     var file = $('#productImage').get(0).files[0];
-    console.log(file);
     //If file var exists then execute File Storage Creation
     Meteor.call('addProduct', file, name, category, description, is_featured)
     //Clear form
