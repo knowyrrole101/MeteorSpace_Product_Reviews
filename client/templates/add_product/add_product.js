ebeec37d461d2f6273eb7536307dd4405dba8479
Moon Ahmed <planetbarsent@gmail.com>
2016-01-19 14:47:09 -0800

    AMAZON s3

--- client/templates/add_product/add_product.js
+++ client/templates/add_product/add_product.js
@@ -8,7 +8,44 @@ Template.add_product.events({
     //Jquery to pull file from form.
     var file = $('#productImage').get(0).files[0];
     //If file var exists then execute File Storage Creation
-    Meteor.call('addProduct',file,name,category,description,is_featured);
+    console.log(file)
+    if (file){
+      fsFile = new FS.File(file);
+      console.log(file);
+      console.log(fsFile);
+      //Insert into ProductsImages Collection
+      //This collection only stores Product Images/Can be all images if necessary
+      ProductsImages.insert(fsFile, function(err, result){
+        if(!err){
+          console.log(result);
+          var productImage = "/cfs/files/ProductsImages/" + result._id;
+          console.log("product imge" + productImage)
+          //Insert into Products Collection
+          Products.insert({
+            name: name,
+            category: category,
+            description: description,
+            is_featured: is_featured,
+            image: productImage,
+            createdAt: new Date()
+          });
+        }
+      });
+    } else {
+      //If no image still add product..but
+      var productImage = "/img/noimage.png";
+      //Insert into Products Collection
+      Products.insert({
+        name: name,
+        category: category,
+        description: description,
+        is_featured: is_featured,
+        image: productImage,
+        createdAt: new Date()
+      });
+    }
+  }
+    // Meteor.call('addProduct',file,name,category,description,is_featured);
     //Clear form
     // event.target.name.value = '';
     // event.target.category.value = '';
