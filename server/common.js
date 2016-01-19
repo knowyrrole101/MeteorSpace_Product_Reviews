bdefa6c8d31b37236eb82328bd338388724edf0c
Moon Ahmed <planetbarsent@gmail.com>
2016-01-18 20:25:14 -0800

    heroku confi

--- server/common.js
+++ server/common.js
@@ -5,12 +5,3 @@ Meteor.publish('products', function(){
 Meteor.publish('categories', function(){
   return Categories.find();
 });
-
-ProductsImages.allow({
-  insert: function(fileId, document){
-    return true;
-  },
-  download: function(fileId, document){
-    return true;
-  }
-});
